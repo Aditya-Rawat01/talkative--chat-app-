296e112d9ea81bcca569d4e248a99bbf252bf5c2
Aditya Rawat <adityarawat240@gmail.com>
2025-02-23 20:40:45 +0530

    check 1

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,7 @@ const app=express()
 const prisma=new PrismaClient()
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
